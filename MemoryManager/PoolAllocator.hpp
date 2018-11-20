16759075332b4c2db11cd24ef9f2401d417303f8
Skratzy <ettafredrik@gmail.com>
2018-11-20 09:51:40 +0100

    Fixed a few items in PoolAllocator

--- MemoryManager/PoolAllocator.hpp
+++ MemoryManager/PoolAllocator.hpp
@@ -8,7 +8,22 @@
 #include "Allocator.h"
 
 class PoolAllocator: private Allocator 
-{	
+{
+public:
+	struct Entry;
+
+private:
+	unsigned int findFreeEntry();
+
+private:
+	// Vector can't be used in conjuction with objects that can't be moved/copied
+	// https://stackoverflow.com/questions/37870731/resize-a-stdvectorstdatomic-bool-assigning-true-to-all-atomic-bools
+	std::deque<std::unique_ptr<Entry>> m_entries;
+
+	size_t m_sizeEachEntry;
+	unsigned int m_numEntries;
+
+
 public:
 	typedef unsigned int ID;
 
@@ -32,18 +47,5 @@ public:
 		for (unsigned int i = 0; i < m_numEntries; i++)
 			std::cout << m_entries[i]->val << std::endl;
 	}
-
-
-private:
-	unsigned int findFreeEntry();
-
-
-private:
-	// Vector can't be used in conjuction with objects that can't be moved/copied
-	// https://stackoverflow.com/questions/37870731/resize-a-stdvectorstdatomic-bool-assigning-true-to-all-atomic-bools
-	std::deque<std::unique_ptr<Entry>> m_entries;
-
-	size_t m_sizeEachEntry;
-	unsigned int m_numEntries;
 
 };
