83522ce0d1cd56dd7450e0f1a6c9d8153ead2090
Oliver <oliverglandberger@gmail.com>
2018-11-21 17:19:44 +0100

    Finished adding all review changes except for mutex lock changes. Those changes are still in psudocode. Will fix that tomorrow early in the morning!

--- MemoryManager/PoolAllocator.cpp
+++ MemoryManager/PoolAllocator.cpp
@@ -1,47 +1,52 @@
 #include "PoolAllocator.hpp"
-
 /*
 	Private Functions
 */
-unsigned int PoolAllocator::findFreeEntry(int quadrent)
+
+// 'quadrent' = Thread ID (0, 1, 2, or 3)
+int PoolAllocator::findFreeEntry(int quadrent)
 {
+	int returnValue;
+
+	void* allocationAddress = m_quadFreeAddress.at(quadrent);
+	// If true = quadrent is already fully allocated!
+	// EARLY EXIT (-1)
+	if (allocationAddress == nullptr)
+		return -1;
+
 	char* tempAddress;
+	// Address of pool's start
+	tempAddress = static_cast<char*>(m_memPtr);
+	// Address of quadrent's start
+	tempAddress += static_cast<int>(static_cast<float>(quadrent) * static_cast<float>(m_sizeBytes) * 0.25f);
+	// Which specific pool entry we are looking at
+	int entryNum = static_cast<int>((static_cast<char*>(allocationAddress) - static_cast<char*>(m_memPtr)) / sizeof(char));
+	returnValue = entryNum;
+	// Set that entry to 'allocated'
+	m_entries.at(entryNum) = true;
 
-	tempAddress = (char*)this->m_memPtr;
-	tempAddress += (int)((float)quadrent * ((float)this->m_sizeBytes * 0.25f));
-	
-	void* startAddress = (void*)tempAddress;
-	void* stopAddress = (void*)(tempAddress += (int)((float)this->m_sizeBytes * 0.25f));
-	void* allocationAddress = this->quadFreeAddress.at(quadrent);
-	
-	int entryNum = ((char*)(this->m_memPtr) - (char*)(allocationAddress));
-	this->m_entries.at(entryNum)->used = true;
+	tempAddress = static_cast<char*>(m_quadFreeAddress.at(quadrent));
 
-	tempAddress = (char*)this->quadFreeAddress.at(quadrent);
-	while (this->m_entries.at(entryNum)->used == true)
+	void* startAddress = static_cast<void*>(tempAddress);
+	void* stopAddress = static_cast<void*>(tempAddress += static_cast<int>(static_cast<float>(m_sizeBytes) * 0.25f));
+	// We are looking for the next free entry
+	while (m_entries.at(entryNum) == true)
 	{
 		tempAddress++;
 		entryNum++;
 
-		// If reached quadrant end; search from quadrant start
+		// If reached quadrant end...
 		if (tempAddress == stopAddress)
-			tempAddress = (char*)startAddress;
+			// ... search from quadrant start
+			tempAddress = static_cast<char*>(startAddress);
 		// If reaching where we started = quadrant is completely full!
 		if (tempAddress == allocationAddress)
-		{
-			this->quadFreeAddress.at(quadrent) = nullptr;
-			break;
+		{	// '== nullptr' means that the memory is full.
+			m_quadFreeAddress.at(quadrent) = nullptr;
 		}
 	}
-
-	//for (unsigned int i = 0; i < m_entries.size(); i++) {
-	//	bool expected = false;
-	//	// Atomic compare and exchange, returns true if 'used == expected', else puts whatever 
-	//	// 'used' was into 'expected' and tries again
-	//	if (m_entries[i]->used.compare_exchange_strong(expected, true))
-	//		return i;
-	//}
-	return -1;
+	// 'returnValue' = 'entryNum'
+	return returnValue;
 }
 
 /*
@@ -55,65 +60,87 @@ PoolAllocator::PoolAllocator(void* memPtr, unsigned int entrySize, unsigned int 
 	m_numEntries = numEntries;
 
 	for (unsigned int i = 0; i < numEntries; i++)
-		m_entries.emplace_back(std::unique_ptr<Entry>(new Entry()));
+		m_entries.emplace_back(false);
+
+	m_usedMtxs.resize(4);
+	for (int i = 0; i < m_usedMtxs.size(); i++)
+		m_usedMtxs[i] = ATOMIC_VAR_INIT(false);
 }
 
 PoolAllocator::~PoolAllocator()
 {
-	this->cleanUp();
+	cleanUp();
 }
 
 void * PoolAllocator::allocate(int quadrant)
 {
+	std::shared_lock<std::shared_mutex> lock(m_mtx);
+	// ALTERNATIVE #1: A size4 array of atomic bools for each quadrant
+
+	bool expected = false;
+	while (!m_usedMtxs.at(quadrant).compare_exchange_strong(expected, true))
+		// DO STUFF
+	/*
+	
+	forloop(4 times, for 4 quadrants)
+	{
+		while (bool (i) is locked)
+			do nothing;
+
+		if (bool(i) is not locked)
+		{
+			lock bool(i) and check that quadrant
+		}
+	}
+
+	*/
 	unsigned int index = PoolAllocator::findFreeEntry(quadrant);
-
-	if (index == -1) {
-		// GET SOME MOH MEM BICH
-		return nullptr;
-	}
-	else {
-		// Makes sure any access to this entry in the memory is unaccessible by other threads
-		std::unique_lock<std::shared_mutex> lock_guard(m_entries[index]->mu);
-
-		m_entries[index]->val = std::this_thread::get_id();
-
-		// Return memory adress for the entry (memory_bottom_pointer + entry * size_of_entry)
-		return static_cast<char*>(m_memPtr) + index * m_entrySize;
-	}
 }
 
 void PoolAllocator::deallocateAll()
 {
-	for (auto& i : this->m_entries)
-		i->used = false;
+	std::unique_lock<std::shared_mutex> lock(m_mtx);
+
+	for (auto& i : m_entries)
+		i = false;
 
 	// Set all new freeEntries (each quadrant)
+	int quadrantSize = (int)((float)m_numEntries * 0.25f);
+	
+	for (int i = 0; i < 4; i++)
+		m_quadFreeAddress.at(i) = (static_cast<char*>(m_memPtr) + (quadrantSize * i));
 }
 
 void PoolAllocator::deallocateSingle(void* address)
-{	// STEP 1
+{	/// STEP 1
 	char* startPoint;
 	char* endPoint;
 
-	startPoint = (char*)this->m_memPtr;
-	endPoint = (char*)address;
+	startPoint = static_cast<char*>(m_memPtr);
+	endPoint = static_cast<char*>(address);
+	// Calculates which entry we are deallocating
+	int entryIndex = static_cast<int>(static_cast<float>((endPoint - startPoint) / static_cast<float>(sizeof(char))));
+	// Setting the entry to false = deallocation
+	m_entries.at(entryIndex) = false;
 
-	int entryIndex = (endPoint - startPoint);
-	this->m_entries.at(entryIndex)->used = false;
-
-	// STEP 2
-	int quadrantSize = (int)((float)this->m_numEntries * 0.25f);
-	int currentQuadrant = (entryIndex / quadrantSize);
-
-	if (this->quadFreeAddress.at(currentQuadrant) == nullptr)
-		this->quadFreeAddress.at(currentQuadrant) = address;
+	/// STEP 2
+	// Calculate the size of a quadrant
+	int quadrantSize = static_cast<int>(static_cast<float>(m_numEntries * 0.25f));
+	// Check which quadrant we are in
+	int currentQuadrant = static_cast<int>(static_cast<float>(entryIndex / quadrantSize));
+	// Set that specific quadrant's newest free entry to the one we
+	// just deallocated.
+	
+	// bool expected = false;
+	// while (!atomic bool array [i].compare_exchange_strong(expected, true))
+	m_quadFreeAddress.at(currentQuadrant) = address;
 }
 
 void PoolAllocator::cleanUp()
 {
-	if (this->m_memPtr != nullptr)
+	if (m_memPtr != nullptr)
 	{
-		free(this->m_memPtr);
-		this->m_memPtr = nullptr;
+		free(m_memPtr);
+		m_memPtr = nullptr;
 	}
 }
--- MemoryManager/PoolAllocator.hpp
+++ MemoryManager/PoolAllocator.hpp
@@ -9,15 +9,26 @@
 #include "Allocator.h"
 
 class PoolAllocator: private Allocator 
-{	
+{
+
+private:
+	int findFreeEntry(int quadrant);
+
+private:
+	// Vector can't be used in conjunction with objects that can't be moved/copied
+	// https://stackoverflow.com/questions/37870731/resize-a-stdvectorstdatomic-bool-assigning-true-to-all-atomic-bools
+	std::deque<bool> m_entries;
+
+	size_t m_entrySize;
+	unsigned int m_numEntries;
+
+	std::shared_mutex m_mtx;
+	std::vector<std::atomic_bool> m_usedMtxs;
+
+	std::vector<void*> m_quadFreeAddress;
+
 public:
 	typedef unsigned int ID;
-
-	struct Entry {
-		//std::shared_mutex mu; // Used to make sure that the resource isn't read while it's being modified
-		std::atomic_bool used = ATOMIC_VAR_INIT(false); // Used for atomic compare and exchange
-		//std::thread::id val = std::this_thread::get_id(); // Temporary for testing
-	};
 
 public:
 	PoolAllocator(void* memPtr, unsigned int entrySize, unsigned int numEntries);
@@ -32,21 +43,8 @@ public:
 	void printEntries() {
 		for (unsigned int i = 0; i < m_numEntries; i++)
 			//std::cout << m_entries[i]->val << std::endl;
-			std::cout << (int)m_entries[i]->used << "\n";
+			std::cout << (int)m_entries[i] << "\n";
 	}
 
 	void cleanUp();
-		
-private:
-	unsigned int findFreeEntry(int quadrant);
-
-private:
-	// Vector can't be used in conjunction with objects that can't be moved/copied
-	// https://stackoverflow.com/questions/37870731/resize-a-stdvectorstdatomic-bool-assigning-true-to-all-atomic-bools
-	std::deque<std::unique_ptr<Entry>> m_entries;
-
-	size_t m_entrySize;
-	unsigned int m_numEntries;
-
-	std::vector<void*> quadFreeAddress;
 };
